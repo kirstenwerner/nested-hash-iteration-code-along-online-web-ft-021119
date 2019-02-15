2a7a72f0a6dec99c8a309b917d689d208885da90
Kirsten Werner <kirstenmwerner@gmail.com>
2019-02-15 02:56:30 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,9 +15,7 @@ require 'pry'
   }
   
 def remove_strawberry(contacts)
-  contacts["Freddy Mercury"][:favorite_ice_cream_flavors].each do |flavor| 
-    flavor.delete('strawberry')
-  end 
+  contacts["Freddy Mercury"][:favorite_ice_cream_flavors].delete_if("strawberry")
 end
 
 remove_strawberry(contacts)
